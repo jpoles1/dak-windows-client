19fdaa31806e503ee82b806fd151c14f1792d5b5
Jordan Poles <jpoles1@gmail.com>
2019-11-03 20:31:35 -0500

    Adding loop?

--- src/main.rs
+++ src/main.rs
@@ -34,26 +34,27 @@ fn sleep_windows() {
 fn main () {
     // Load in config
     let client_config = load_config("config.json");
-
-    // Connect to the url and call the closure
-    let url = client_config.ws_url + "?room=" + &client_config.room[..] + "&key=" + &client_config.key[..];
-    let (mut socket, response) = connect(Url::parse(&url[..]).unwrap()).expect("Can't connect to websocket server");
-
-    println!("Connected to the server");
-    println!("Response HTTP code: {}", response.code);
-    println!("Response contains the following headers:");
-    for &(ref header, _ /*value*/) in response.headers.iter() {
-        println!("* {}", header);
-    }
-
     loop {
-        let msg = socket.read_message().expect("Error reading message");
-        if msg.is_text() {
-            let msg_txt = msg.into_text().unwrap();
-            let command: Vec<&str> = msg_txt.split(":").collect();
-            if command.len() == 4 && command[0] == "alexaevent" && command[1] == "computer" {
-                if command[2] == "power" && command[3] == "off" {
-                    sleep_windows();
+        // Connect to the url and call the closure
+        let url = client_config.ws_url + "?room=" + &client_config.room[..] + "&key=" + &client_config.key[..];
+        let (mut socket, response) = connect(Url::parse(&url[..]).unwrap()).expect("Can't connect to websocket server");
+
+        println!("Connected to the server");
+        println!("Response HTTP code: {}", response.code);
+        println!("Response contains the following headers:");
+        for &(ref header, _ /*value*/) in response.headers.iter() {
+            println!("* {}", header);
+        }
+
+        loop {
+            let msg = socket.read_message().expect("Error reading message");
+            if msg.is_text() {
+                let msg_txt = msg.into_text().unwrap();
+                let command: Vec<&str> = msg_txt.split(":").collect();
+                if command.len() == 4 && command[0] == "alexaevent" && command[1] == "computer" {
+                    if command[2] == "power" && command[3] == "off" {
+                        sleep_windows();
+                    }
                 }
             }
         }
